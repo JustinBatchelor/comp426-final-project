db654173f0e06058e73b6d35b70acbfe08fb1ba1
Justin  Batchelor <justinbatchelor@Justins-MacBook-Pro.local>
2020-10-20 00:51:04 -0400

    Initial commit

---
+++ src/auth.js
@@ -0,0 +1,49 @@
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //for logged in user
+    } else {
+        //for logged out user
+    }
+})
+
+const signupForm = document.querySelector('#signup-form');
+const signupButton = document.querySelector('#signup-button');
+const logout = document.querySelector('#logout');
+const loginForm = document.querySelector('#login-form');
+const loginButton = document.querySelector('#login-button');
+
+signupButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            name: signupForm['signup-name'].value});
+    }).then(() => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+})
+
+logout.addEventListener('click', (event) => {
+    event.preventDefault();
+    auth.signOut();
+})
+
+loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+})
